b675027c8f48b3101799c007e04c01ae54aab520
gvituri <gustavovituri@gmail.com>
2021-08-13 15:10:35 -0300

    Add main game loop and player class

---
+++ main.rb
@@ -0,0 +1,19 @@
+=begin
+    Tic Tac Toe
+
+=end
+def print_board(play_sheet)
+    HEADER = "   A   B   C "
+    ROW_SEPARATOR = "  ---|---|---"
+end
+
+TEST_MATRIX = [
+    "   A   B   C ",
+    "0  - | - | - ",
+    "  ---|---|---",
+    "1  - | - | - ",
+    "  ---|---|---",
+    "2  - | - | - "
+]
+
+puts TEST_MATRIX
