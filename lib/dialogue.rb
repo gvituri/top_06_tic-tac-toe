60a1ecc08d6c9b3a6d687766236c5e1c25117e63
gvituri <gustavovituri@gmail.com>
2021-08-16 10:18:49 -0300

    Refactor dialogue system

--- lib/dialogue.rb
+++ lib/dialogue.rb
@@ -1,61 +1,52 @@
 module Dialogue
 
+    @@Dialogue_pool = {
+        welcome: [["none"],
+            ["Welcome!",
+            "Hit enter to start a match."]],
+        setup_mode: [["pvp", "pvc"],
+            ["Chose the match mode:",
+            "pvp - player vs player",
+            "pvc - playes vc computer"]],
+        setup_computer: [["easy", "medium", "hard"],
+            ["Chose the computer dificulty:",
+            "easy",
+            "medium",
+            "hard"]],
+        setup_rounds: [["1", "2", "3", "4", "5"],
+            ["Chose the number of rounds between 1 and 5:"]],
+    }
+
     def self.check_input(input, dictionary)
-        dictionary.each do |word|
-            return true if input == word
-        end
-
-        puts "\nERROR! Input has no match."
-        puts "Please, type either of the following:"
-        dictionary.each do |word|
-            puts word
-        end
-        puts "\n"
-        return false
-    end
-    
-    def self.welcome
-        puts "Welcome!"
-    end
-
-    def self.setup_match_mode
-        entry_dictionary = ["pvp", "pvc"]
-        input = nil
-
         loop do
-            puts "Chose the match mode:"
-            puts "pvp - player vs player"
-            puts "pvc - playes vc computer"
+            dictionary.each do |word|
+                return word if input == word
+            end
+
+            puts "\nERROR! Input has no match."
+            puts "Please, type either of the following:"
+            dictionary.each do |word|
+                puts word
+            end
+            puts "\n"
+
             input = gets.chomp
-            break if check_input(input, entry_dictionary)
         end
-
-        return input
     end
 
-    def self.setup_computer
-        entry_dictionary = ["easy", "medium", "hard"]
-        input = nil
-
-        loop do
-            puts "Chose the computer dificulty between easy, medium and hard:"
-            input = gets.chomp
-            break if check_input(input, entry_dictionary)
+    def self.print_dialogue(dialogue_section)
+        dictionary_array = @@Dialogue_pool[dialogue_section][0]
+        dialogue_array = @@Dialogue_pool[dialogue_section][1]
+        
+        dialogue_array.each do |phrase|
+            puts phrase
         end
 
-        return input
-    end
+        player_input = gets.chomp
 
-    def self.setup_rounds
-        entry_dictionary = [1, 2, 3, 4, 5]
-        input = nil
-
-        loop do
-            puts "Chose the number of rounds between 1 and 5:"
-            input = gets.chomp.to_i
-            break if check_input(input, entry_dictionary)
+        unless dictionary_array[0] == "none"
+            player_input = check_input(player_input, dictionary_array)
+            return player_input
         end
-
-        return input
     end
 end
--- lib/display.rb
+++ lib/display.rb
@@ -10,26 +10,17 @@ module Display
 
     def self.welcome
         clear_display
-        Dialogue.welcome
+        Dialogue.print_dialogue(:welcome)
     end
 
     def self.setup_match
-        clear_display
-        match_mode = Dialogue.setup_match_mode
-        clear_display
-        puts "Mode: #{match_mode}"
+        match_mode = Dialogue.print_dialogue(:setup_mode)
         if match_mode == "pvc"
-            computer_dificulty = Dialogue.setup_computer
+        match_computer = Dialogue.print_dialogue(:setup_computer)
         end
-        clear_display
-        puts "Mode: #{match_mode}"
-        if match_mode == "pvc" 
-        puts "Computer dificulty: #{computer_dificulty}" 
-        end
-        round_number = Dialogue.setup_rounds
+        match_rounds  = Dialogue.print_dialogue(:setup_rounds)
 
-        return Match.new(match_mode, computer_dificulty, round_number)
+        return Match.new(match_mode, match_computer, match_rounds)
     end
 
 end
-
